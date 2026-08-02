648fdbd16a4a4da5fc86e4278efe8f79d2089cf8
Gustavo <gustavo.aranda.sccp@gmail.com>
2026-08-02 19:33:31 -0300

    feat: deploy de lambda para procura de lobbies

---
+++ src/handlers/search_lobby.py
@@ -0,0 +1,53 @@
+import boto3
+import json
+from src.utils.CustomEncoder import CustomEncoder
+from boto3.dynamodb.conditions import Key
+
+resource = boto3.resource('dynamodb')
+lobbies_table = resource.Table('lobbies')
+
+def handler(event, context):
+    query_params = event.get('queryStringParameters') or {}
+    
+    is_valid, result = validateLocationFields(query_params)
+    if not is_valid:
+        return result
+    
+    search_results = searchLobbiesByLocation(result)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(search_results, cls=CustomEncoder)
+    }
+    
+
+def validateLocationFields(query_params):
+    cidade = query_params.get('city')
+    estado = query_params.get('state')
+    esporte = query_params.get('sport_lobby')
+
+    if not estado:
+        return False,{
+            'statusCode': 400, 
+            'body': json.dumps({'message': 'O parâmetro "state" é obrigatório.'})
+        }
+
+
+    sk_prefix = f"LOC#{estado.upper()}#"
+
+    if cidade:
+        cidade_formatada = cidade.strip().lower().replace(' ', '_')
+        sk_prefix += f"{cidade_formatada}#SPORT#"
+
+        if esporte:
+            esporte_formatado = esporte.strip().upper()
+            sk_prefix += f"{esporte_formatado}#"
+    
+    return True, sk_prefix
+
+def searchLobbiesByLocation(sk_prefix):
+    response = lobbies_table.query(
+        IndexName='GSI1',
+        KeyConditionExpression=Key('GSI1_PK').eq('STATUS#OPEN') & Key('GSI1_SK').begins_with(sk_prefix)
+    )
+    return response.get('Items', [])
